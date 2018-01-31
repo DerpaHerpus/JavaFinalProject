8c6b200eebbb2461c3bffbf59f2b18b051090854
David <david2631@gmail.com>
2018-01-31 11:47:15 -0500

    2

--- Player.java
+++ Player.java
@@ -8,4 +8,20 @@ public class Player extends Sprite implements commons
 
   private final String playerImg = ;//PUT PLAYER SPIRTE HERE
   private int width;
+
+  public Player()
+  {
+    initPlayer();
+  }
+
+  private void initPlayer()
+  {
+    ImageIcon ii = new ImageIcon(playerImg);
+    width = ii.getimage().getWidth(null);
+
+    setImage(ii.getImage());
+    setX(START_X);
+    setY(START_Y);
+  }
+
 }
