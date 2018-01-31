d6ab8787e13ba2b8b9d3d3672a67533f5ad34f54
David Tang <davidtang18@yahoo.com>
2018-01-31 10:55:52 -0500

    3

---
+++ Constants.java
@@ -0,0 +1,16 @@
+public interface Commons
+{
+  public static final int MAP_WIDTH = 358;
+  public static final int MAP_HEIGHT = 350;
+  public static final int FLOOR = 290;
+  public static final int ENEMY_HEIGHT = 12;
+  public static final int ENEMY_WIDTH = 12;
+  public static final int MAP_RIGHT = 30;
+  public static final int MAP_LEFT = 5;
+  public static final int GO_DOWN = 15;
+  public static final int NUMBER_OF_ENEMIES = 24;
+  public static final int CHANCE = 5;
+  public static final int DELAY = 17;
+  public static final int PLAYER_WIDTH = 15;
+  public static final int PLAYER_HEIGHT = 10;
+}
