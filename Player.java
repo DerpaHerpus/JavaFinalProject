606998ce5126ff37ce2097efb5f46b1b2d798470
David <david2631@gmail.com>
2018-01-31 11:21:04 -0500

    1

---
+++ Player.java
@@ -0,0 +1,11 @@
+import java.awt.event.KeyEvent;
+import java.swing.ImageIcon;
+
+public class Player extends Sprite implements commons
+{
+  private final int START_Y = 280;
+  private final int START_X = 270;
+
+  private final String playerImg = ;//PUT PLAYER SPIRTE HERE
+  private int width;
+}
