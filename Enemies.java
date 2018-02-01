c65aa1d9774c985d2dfe5617256fecc998862ea0
David <david2631@gmail.com>
2018-02-01 09:28:05 -0500

    3

--- Enemies.java
+++ Enemies.java
@@ -4,4 +4,52 @@ public class Enemies extends Sprite
 {
   private EnemyShot enemyShot;
   private final String enemySprite = "C:\Users\David Tang\Videos\Enemy.png";
+
+  public Enemies(int x, int y)
+  {
+    initEnemies(x, y);
+  }
+  private void initEnemies(int x, int y)
+  {
+    this.x = x;
+    this.y = y;
+
+    enemyShot = new EnemyShot(x, y);
+    ImageIcon ii = new Image Icon(enemySprite);
+    setImage(ii.getImage());
+  }
+  public void act(int direction)
+  {
+    this.x += direction;
+  }
+  public EnemyShot getEnemyShot()
+  {
+    return enemyShot;
+  }
+  public class EnemyShot extends Sprite
+  {
+    private final String enemyShotImg = //Put enemy shot sprite here;
+    private boolean destroyed;
+
+    public EnemyShot(int x, int y)
+    {
+      initEnemyShot(x, y);
+    }
+    private void initEnemyShot(int x, int y)
+    {
+      setDestroyed(true);
+      this.x = x;
+      this.y = y;
+      ImageIcon ii = new ImageIcon(enemyShotImg);
+      setImage(ii.getImage());
+    }
+    public void setDestroyed(boolean destroyed)
+    {
+      this.destroyed = destroyed;
+    }
+    public boolean isDestroyed()
+    {
+      return destroyed;
+    }
+  }
 }
