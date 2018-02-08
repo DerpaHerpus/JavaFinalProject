10f9e7b0b650901b3fc1e50cf3547a5a3f68e05d
DerpaHerpus <32910135+DerpaHerpus@users.noreply.github.com>
2018-02-08 00:41:08 -0500

    24

--- Enemies.java
+++ Enemies.java
@@ -1,59 +1,65 @@
-import java.awt.event.*;
-import java.util.Random;
-import javax.swing.*;
-import java.awt.*;
+package com.zetcode;
 
-public class Enemies extends JPanel
-{
-  private int size, x, y, dx, dy, bx, by, dbx, dby, numBombs;
-  private Random gen;
-  private Image im;
-  //private Image enemyImg;
-  //Image enemyImg = new Image("Enemy.png");
+import javax.swing.ImageIcon;
 
-  Image enemyImg = new Image("Enemy.png");
+public class Enemies extends Sprite {
 
-  public Enemies(int x1, int y1, string f)
-  {
-    size = 25;
-    dx = 0;
-    dy = 0;
-    bx = -1;
-    by = -1;
-    dbx = 2;
-    dby = 2;
-    x = x1;
-    y = y1;
-    numBombs = 5;
-  }
-  public void moveLeft()
-  {
-    dx = -1;
-    if (x<=5)
-      x = 850;
-  }
-  public void moveRight()
-  {
-    dx = 1;
-    if (x >= 850)
-      x = 6;
-  }
-  public void draw(Graphics gr)
-  {
-    x += dx;
-      gr.drawImage(im, x, y, size, size, Color.black, this);
-  }
+    private Bomb bomb;
+    private final String EnemyImg = "spritey bois/Enemy.png";
 
-  public int getX()
-  {
-    return x;
-  }
-  public int getY()
-  {
-    return y;
-  }
-  public int getBY()
-  {
-    return by;
-  }
+    public Enemy(int x, int y) {
+
+        initEnemy(x, y);
+    }
+
+    private void initEnemy(int x, int y) {
+
+        this.x = x;
+        this.y = y;
+
+        bomb = new Bomb(x, y);
+        ImageIcon ii = new ImageIcon(EnemyImg);
+        setImage(ii.getImage());
+    }
+
+    public void act(int direction) {
+
+        this.x += direction;
+    }
+
+    public Bomb getBomb() {
+
+        return bomb;
+    }
+
+    public class Bomb extends Sprite {
+
+        private final String bombImg = "spritey bois/Hhot.png";
+        private boolean destroyed;
+
+        public Bomb(int x, int y) {
+
+            initBomb(x, y);
+        }
+
+        private void initBomb(int x, int y) {
+
+            setDestroyed(true);
+            this.x = x;
+            this.y = y;
+            ImageIcon ii = new ImageIcon(bombImg);
+            setImage(ii.getImage());
+
+        }
+
+        public void setDestroyed(boolean destroyed) {
+
+            this.destroyed = destroyed;
+        }
+
+        public boolean isDestroyed() {
+
+            return destroyed;
+        }
+    }
 }
